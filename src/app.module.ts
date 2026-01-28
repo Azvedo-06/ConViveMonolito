5bd785639ad161bbfab95f0970a5468348831257
Azvedo-06 <azinho006@gmail.com>
2026-01-27 22:41:33 -0300

    sequelize migrations e data base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { APP_GUARD } from '@nestjs/core';
 import { AdminModule } from './admin/admin.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // Faze o módulo de configuração estar disponível globalmente
+    }),
+    SequelizeModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        dialect: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: Number(config.get('DB_PORT')),
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        autoLoadModels: true, // Carrega os modelos automaticamente
+        synchronize: false, // SEMPRE false com migrations
+      }),
     }),
     AuthModule,
     AdminModule,
